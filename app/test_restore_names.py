d4dd84552a9fc99d663a49aef81a326cefff5e45
Denys Dumanetskyi <dumaneckijdenis@gmail.com>
2026-03-02 14:05:40 +0200

    Solve flake8 problems.

--- app/test_restore_names.py
+++ app/test_restore_names.py
@@ -1,4 +1,3 @@
-import pytest
 from app.restore_names import restore_names
 
 
@@ -6,30 +5,30 @@ def test_restore_names_when_first_name_is_missing() -> None:
     users = [
         {"last_name": "Adams",
          "full_name": "Mike Adams",
-        }
-    ]
+        }]
     restore_names(users)
     assert users[0]["first_name"] == "Mike"
+
 
 def test_restore_names_when_first_name_is_none() -> None:
     users = [
         {"first_name": None,
          "second_name": "Smith",
          "full_name" : "John Smith",
-        }
-    ]
+        }]
     restore_names(users)
     assert users[0]["first_name"] == "John"
+
 
 def test_should_not_change_existing_first_name() -> None:
     users = [
         {"first_name" : "Russel",
          "second_name" : "Crawl",
          "full_name" : "Russel Crawl",
-        }
-    ]
+        }]
     restore_names(users)
     assert users[0]["first_name"] == "Russel"
+
 
 def test_restore_names_for_multiple_users() -> None:
     users = [
